a360155d2a429fb662c7e98a97cde8faabe09f34
taisoliva <tais15oliva@gmail.com>
2023-04-06 13:59:13 -0300

    POST: tweets

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from "express"
 const app = express()
 const PORT = 5000;
 const user = []
+const tweets = []
 app.use(express.json())
 
 app.get("/", (req,res) => {
@@ -17,4 +18,18 @@ app.post("/sign-up", (req, res) =>{
     res.send("OK")
 })
 
-app.listen(PORT, ()=> console.log(`Escutando na porta ${PORT}`))
+app.post("/tweets", (req,res) => {
+    const {username, tweet} = req.body
+    console.log(username)
+
+   if( (user.find((item) => item.username === username )) !== undefined){
+        const novoTweet = {username: username, tweet:tweet}
+        tweets.push(novoTweet)
+        res.status(200).send("OK")
+   } else{
+    res.status(400).send("UNAUTHORIZED")
+   }
+    
+})
+
+app.listen(PORT, ()=> console.log(`Escutando naw porta ${PORT}`))
