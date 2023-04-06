812b587e0ad21209ace035a42045dc169cee3a23
taisoliva <tais15oliva@gmail.com>
2023-04-06 12:39:19 -0300

    POST: Sign-up

--- src/app.js
+++ src/app.js
@@ -2,9 +2,19 @@ import express from "express"
 
 const app = express()
 const PORT = 5000;
+const user = []
+app.use(express.json())
 
 app.get("/", (req,res) => {
-    res.send("Ola vc acessou sua API")
+    res.send(user)
+})
+
+app.post("/sign-up", (req, res) =>{
+    const {username, avatar} = req.body
+
+    console.log(req.body)
+    user.push({username:username, avatar:avatar})
+    res.send("OK")
 })
 
 app.listen(PORT, ()=> console.log(`Escutando na porta ${PORT}`))
